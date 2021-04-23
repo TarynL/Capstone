23390b39e79fe0f3064ff6ce64f22a9b451f5bcd
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-04-23 11:57:59 -0500

    privateList is populating

--- src/components/recyclables/PrivateList.js
+++ src/components/recyclables/PrivateList.js
@@ -1,4 +1,33 @@
-// import React, { useState, useEffect} from 'react';
-// import {PrivateCard} from './PrivateCard';
-// import {getMyRecyclables} from '../../modules/PrivateListManager';
+import React, { useState, useEffect} from 'react';
+import {PrivateCard} from './PrivateCard';
+import {getMyRecyclables} from '../../modules/PrivateListManager';
 // import {useHistory} from 'react-router-dom'
+
+export const PrivateList = () => {
+
+    const [privates, setPrivates] = useState([]);
+    // const history = useHistory();
+
+    const getRecyclables = () => {
+        return getMyRecyclables()
+        .then(res => {
+            setPrivates(res)
+        });
+    };
+
+    useEffect(() => {
+        getRecyclables();
+    }, []);
+
+    return(
+        <>
+        <div className="container-cards">
+        {privates.map(recyclable =>
+          <PrivateCard
+            key={recyclable.id}
+            recyclable={recyclable}
+            />)}
+      </div>
+        </>
+    )
+}
